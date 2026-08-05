64340ce5b5a7f2d33e5e01a9589089afa68e28a1
ALWIN NITHISH <168280244+alwinimaginer@users.noreply.github.com>
2026-08-05 16:12:50 +0530

    Create P5_UART_POLLING_password.h

---
+++ P5_UART_POLLING_password.h
@@ -0,0 +1,15 @@
+/*
+ * password.h
+ *
+ *  Created on: 09-Jul-2026
+ *      Author: Lenovo
+ */
+
+#ifndef PASSWORD_H_
+#define PASSWORD_H_
+
+#include<stdint.h>
+
+uint8_t Password_Check(const char *Userpassword);
+
+#endif /* PASSWORD_H_ */
